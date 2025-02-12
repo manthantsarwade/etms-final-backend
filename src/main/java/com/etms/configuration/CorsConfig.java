be5b902d186ec61a571e602e98b519367b3de971
Manthan Sarawade <manthantsarwade@outlook.com>
2025-02-12 23:50:00 +0530

    changed buildspec

--- src/main/java/com/etms/configuration/CorsConfig.java
+++ src/main/java/com/etms/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","etmsproject.duckdns.org") 
+                        .allowedOrigins("http://localhost:5173","http://etmsproject.duckdns.org") 
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
